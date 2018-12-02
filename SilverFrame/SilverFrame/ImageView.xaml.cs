1679e833320f124be02a57088af3a1c2224d0d49
Josh Toye <jtoye@harding.edu>
2018-12-02 16:49:06 -0600

    Can now download and write a picture to disk

--- SilverFrame/SilverFrame/ImageView.xaml.cs
+++ SilverFrame/SilverFrame/ImageView.xaml.cs
@@ -30,19 +30,16 @@ namespace SilverFrame
             this.InitializeComponent();
             ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
             RotatingImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/images/test.JPG"));
-            //handleShow();
+            handleShowAsync();
             //RotatingImage.
         }
 
-        public void handleShow()
+        public async System.Threading.Tasks.Task handleShowAsync()
         {
-            using (var client = new System.Net.WebClient())
-            //{
-            //    client.DownloadFile("https://lh3.googleusercontent.com/lr/AJ-EwvnYqclSka1f8J-8b1Pooj-8B3JciYhkvI8bjdIakItBv0oMoVu5T7lBXksXdgwmHaMNZED5thJH56UAIGszJRESSkHeltiRgaQ3jf9017W6-ViiYK6dHTeHDZUakfGjiN3ji0RKGdNEZ1xFWk4bVhmWyFAFfN7R0ADq-ig0qL7Lm5oVadKGd4ujYN1rUo22tOsIV4B7BmhlSHuXiFgTvldV2gaxcIpJVY5LVIZkxv1abEuheizW_HNlDjqavdcG3bbzi3xLakcTmqn8frBsOiRD1Q6irIPqzy1t44xMWvCmGec3_IN2jRXd7uABzcdSJPZgdR8SnE5EdlOgDBbznNwl8gqoSfcMOiXz4-NHkK0cYv2_YjyqrzRH7jzxkMuV_9U9r3rpNWb23FZzoEd6eAGZpPi0nlIqAPaPQGW23YA21jMVwbOlj97-tLVerUgcZezCzOGjKzWEdWBrDoTD2gMvAMcAekqNQXHFPBOmUFOS0FYpnAMxh7mOBEUQfobcNXktTtC2KNhZwnJeXm1y03u3KkIpf3C_QBSTUNcFQ5Wld7RC9nR4JXDYkf-2fBvwI9OloZTQgnYhmsC5qMIJIE48XbPYkS_IpXXuFlU-oVOt79aVhxnLC7pRMVy5Jd5us56pO_ytk_wwKb_tANBm1BKC2A7wuY8qcHuxT5MkcW-7yBKHxW6cWuQuqSpmpQwUWfQJXC1gXIC7v5n4P4HFQmgrz7daYbTlc-fNPf-6UiqmBKa8Qa8-itQT7rGaXPbHz57J5eviPqkMubmGZzqYfoaJgaroftGanJAsOYmN-KJkNBuPkaE", @"C://SilverFrame/a.jpg");
-            //}
+            Uri temp = new Uri("https://m.media-amazon.com/images/M/MV5BMTQ1OTM0NDM1NV5BMl5BanBnXkFtZTgwOTUxOTU2NzE@._V1_SY1000_CR0,0,707,1000_AL_.jpg");
+            await ImageLoader.LoadImage(temp, "bob.jpg");
 
-
-            RotatingImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/images/test2.jpg"));
+            //RotatingImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/images/test2.jpg"));
             //BitmapImage bi = new BitmapImage();
             //bi.UriSource = new Uri(@"ms-appx:/Assets/images/test2.jpg", UriKind.RelativeOrAbsolute);
 
@@ -53,46 +50,46 @@ namespace SilverFrame
 
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // Get the Pictures library
-            Windows.Storage.StorageFolder picturesFolder =
-                Windows.Storage.KnownFolders.PicturesLibrary;
-            IReadOnlyList<StorageFolder> folders =
-                await picturesFolder.GetFoldersAsync();
+        //protected async override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    // Get the Pictures library
+        //    Windows.Storage.StorageFolder picturesFolder =
+        //        Windows.Storage.KnownFolders.PicturesLibrary;
+        //    IReadOnlyList<StorageFolder> folders =
+        //        await picturesFolder.GetFoldersAsync();
 
-            // Process file folders
-            foreach (StorageFolder folder in folders)
-            {
-                // Get and process files in folder
-                IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
-                foreach (StorageFile file in fileList)
-                {
-                    Windows.UI.Xaml.Media.Imaging.BitmapImage bitmapImage =
-                        new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+        //    // Process file folders
+        //    foreach (StorageFolder folder in folders)
+        //    {
+        //        // Get and process files in folder
+        //        IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
+        //        foreach (StorageFile file in fileList)
+        //        {
+        //            Windows.UI.Xaml.Media.Imaging.BitmapImage bitmapImage =
+        //                new Windows.UI.Xaml.Media.Imaging.BitmapImage();
 
-                    // Open a stream for the selected file.
-                    // The 'using' block ensures the stream is disposed
-                    // after the image is loaded.
-                    using (Windows.Storage.Streams.IRandomAccessStream fileStream =
-                        await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
-                    {
-                        // Set the image source to the selected bitmap.
-                        Windows.UI.Xaml.Media.Imaging.BitmapImage bmi =
-                            new Windows.UI.Xaml.Media.Imaging.BitmapImage();
-                        bmi.SetSource(fileStream);
+        //            // Open a stream for the selected file.
+        //            // The 'using' block ensures the stream is disposed
+        //            // after the image is loaded.
+        //            using (Windows.Storage.Streams.IRandomAccessStream fileStream =
+        //                await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+        //            {
+        //                // Set the image source to the selected bitmap.
+        //                Windows.UI.Xaml.Media.Imaging.BitmapImage bmi =
+        //                    new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+        //                bmi.SetSource(fileStream);
 
-                        // Create an Image control.  
-                        //Image img = new Image();
-                        //img.Height = 50;
-                        RotatingImage.Source = bmi;
+        //                // Create an Image control.  
+        //                //Image img = new Image();
+        //                //img.Height = 50;
+        //                RotatingImage.Source = bmi;
 
-                        // Add the Image control to the UI. 'imageGrid' is a
-                        // VariableSizedWrapGrid declared in the XAML page.
-                        //imageGrid.Children.Add(img);
-                    }
-                }
-            }
-        }
+        //                // Add the Image control to the UI. 'imageGrid' is a
+        //                // VariableSizedWrapGrid declared in the XAML page.
+        //                //imageGrid.Children.Add(img);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
